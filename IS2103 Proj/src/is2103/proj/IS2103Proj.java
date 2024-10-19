bcf1ba1733f79db673d8753d6c646e9f51e98d98
Mark <baronchew@hotmail.com>
2024-10-19 14:36:31 +0800

    test

---
+++ IS2103 Proj/src/is2103/proj/IS2103Proj.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package is2103.proj;
+
+/**
+ *
+ * @author Mark
+ */
+public class IS2103Proj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    
+}
